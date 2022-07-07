de040cc7c4d3a58cff23532c275472b1e13893e5
Mikhaylov Artem <gits.info20@gmail.com>
2022-07-07 22:10:18 +0300

    seminar_10 ver.1

---
+++ prog_000/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+**Задача 70:** Напишите программу, которая на вход принимает два числа и выдаёт первые N
+чисел, для которых каждое следующее равно сумме двух предыдущих.
+3 и 4, N = 5 -> 3 4 7 11 18
+6 и 10, N = 4 -> 6 10 16 26
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(ReadLine());
+WriteLine("Введите N: ");
+int n = Convert.ToInt32(ReadLine());
+Write($"{a} {b} ");
+PrintSumNumbers(a, b, n - 2);
+
+void PrintSumNumbers(int first, int second, int stop)
+{
+    if (stop > 0)
+    {
+        Write($"{first + second} ");
+        stop--;
+        first += second;
+        PrintSumNumbers(second, first, stop);
+    }
+}
---
+++ prog_002/Program.cs
@@ -0,0 +1,31 @@
+﻿/***Задача 72:** Заданы 2 массива: info и data. В массиве info хранятся двоичные представления
+нескольких чисел (без разделителя). В массиве data хранится информация о количестве бит,
+которые занимают числа из массива info. Напишите программу, которая составит массив десятичных
+представлений чисел массива data с учётом информации из массива info.
+
+Комментарий: первое число занимает 2 бита (01 -> 1); второе число занимает 3 бита (111 -> 7);
+третье число занимает 3 бита (000 -> 0; четвёртое число занимает 1 бит (1 -> 1)
+входные данные:
+data = {0, 1, 1, 1, 1, 0, 0, 0, 1 }
+info = {2, 3, 3, 1 }
+
+выходные данные:
+1, 7, 0, 1
+*/
+
+using System;
+using static System.Console;
+Clear();
+
+int[] GetResultArray(int[] dataArray, int[] infoArray)
+{
+    int[] result = new int[infoArray.Length];
+    string data = String.Join("", dataArray);
+    for (int i = 0; i < result.Length; i++)
+    {
+        string subData = data.Substring(0, infoArray[i]);
+        result[i] = Convert.ToInt32(subData, 2);
+        data = data.Remove(0, infoArray[i]);
+    }
+    return result;
+}
